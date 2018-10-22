c555975e0bd33b8966f8b684ae70b89e418d4364
Unknown <akrasner19@gmail.com>
2018-10-22 17:23:24 -0400

    Adding code. Need to test and create test file.

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-Ventera Challenge
+# Ventera Developer Challenge
+
+My solution is in the file transformData.py. It is written in Python 3
+
+The function transformData takes in a string as the location of the old json, and a string as the location for the new json.
+Calling this function will generate a json file that follows the solution instructions.
+
+Next with what extra time I had, I almost completed the second story from the problem.
+I structured this solution as a class to make it more scalable and modular. I also made the solutions able to answer broader queries than just the ones that were asked.
+
+Call the class method howManySold(itemname) with the parameter of the name of the item you want the data on
+
+Calling the method totalRevenue() will return the total revenue of all the elements
+
+highestRevenueVendor() Gives the vendor from the data with the highest revenue
+
+mostItemInMonthID(itemStr,monthStr) returns the customer ID of whoever bought the most of a specific item in a specific month
+
+All of these functions are demonstrated in test.py
---
+++ transformData.py
@@ -0,0 +1,89 @@
+import json
+
+#Function to transform the data into the new format
+def transformData(jsonFileName, outputFileName):
+	with open(jsonFileName, 'r') as content:
+		jdata = content.read()
+		data = json.loads(jdata)
+		fixedData = []
+		for element in data:
+			fixedElement = {}
+			fixedElement["id"] = element["id"]
+			fixedElement["vendor"] = element["vendor"]
+			fixedElement["date"] = element["date"]
+			fixedElement["customerId"] = element["customer"]["id"]
+			fixedElement["order"] = []
+			for key in element["order"].keys():
+				item = {}
+				item["item"] = key
+				item["quantity"] = element["order"][key]["quantity"]
+				item["price"] = element["order"][key]["price"]
+				item["revenue"] = item["quantity"]*item["price"]
+				fixedElement["order"].append(item)
+			fixedData.append(fixedElement)
+
+		with open(outputFileName, 'w') as outFile:
+			outFile.write(json.dumps(fixedData, indent=1))
+
+#A class that contains the data from a the json
+#allows for access to the underlying data for answering as well as the ability to ex
+class FormattedData:
+	def __init__(self, jsonFileName):
+		with open(jsonFileName, 'r') as content:
+			jdata = content.read()
+			self.data = json.loads(jdata)
+			#Technically the access time for each piece of data could be faster if all of
+			#the relevant questions were calculated here in advance. However, this limits the
+			#scalability of the solution.
+
+	def howManySold(self, itemname):
+		total = 0
+		for element in self.data:
+			for item in element["order"]:
+				if item["item"] == itemname:
+					total += item["quantity"]
+					break
+		return total
+
+	def totalRevenue(self):
+		total = 0
+		for element in self.data:
+			for item in element["order"]:
+				total += item["revenue"]
+		return total
+
+	def highestRevenueVendor(self):
+		vendors = {}
+		for element in self.data:
+			if not element["vendor"] in vendors:
+				vendors[element["vendor"]] = 0
+			for item in element["order"]:
+				vendors[element["vendor"]] += item["revenue"]
+
+		bestRev = 0
+		bestVen = None
+		for key in vendors.keys(): #brute force, could be solved faster given more time
+			if vendors[key] > bestRev:
+				bestRev = 0
+				bestVen = key
+
+		return bestVen
+
+	def mostItemInMonthID(self,itemStr,monthStr):
+		customers = {}
+		for element in self.data:
+			if not element["customerId"] in vendors:
+				customers[element["customerId"]] = 0
+			for item in element["order"]:
+				if item["item"] == itemStr:
+					customers[element["customerId"]] += item["quantity"]
+					break
+
+		bestRev = 0
+		bestVen = None
+		for key in vendors.keys(): #brute force, could be solved faster given more time
+			if vendors[key] > bestRev:
+				bestRev = 0
+				bestVen = key
+
+		return bestVen
